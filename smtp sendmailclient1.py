6ad57c7f03a403a5947c01d4673d1c70053715f0
sanjay <151489019+SanjYE@users.noreply.github.com>
2024-08-29 18:09:05 +0530

    small changes

--- smtp sendmailclient1.py
+++ smtp sendmailclient1.py
@@ -1,13 +1,10 @@
 import socket
 import json
 
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 
 server_address = ('localhost',  8080)
 client_socket.connect(server_address)
-
 
 data = {
     "action": "send_email",
@@ -15,19 +12,12 @@ data = {
     "subject": "Test Email",
     "message": "This is a test email.",
     "attachment": "*********************************" ,  #starred for privacy reasons. add the path of the file you want to attach here
-     
-
 }
-
 
 data_str = json.dumps(data)
 data_bytes = data_str.encode('utf-8')
 client_socket.send(data_bytes)
 
-
 response = client_socket.recv(1024).decode('utf-8')
-
-
 client_socket.close()
-
 print(response)
